f1bd98f9edb6f235139e4739cc18b7fde098d605
milkbread <milkbread@freenet.de>
2014-05-13 13:05:42 +0200

    Try solving coding and

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 [Basics](http://docs.python-guide.org/en/latest/scenarios/scrape/)
 [get HREF attribute](http://stackoverflow.com/questions/14119590/scraperwiki-lxml-how-to-get-the-href-attribute-of-a-child-of-an-element-with)
 [slice an array](http://stackoverflow.com/questions/509211/pythons-slice-notation)
+
+[Encoding](http://www.pythoncentral.io/python-unicode-encode-decode-strings-python-2x/)
+[Encoding for file writing](http://stackoverflow.com/questions/934160/write-to-utf-8-file-in-python)
--- scrap.py
+++ scrap.py
@@ -5,6 +5,7 @@ from lxml import html
 import requests
 import json
 import copy
+import codecs
 
 COLLECTION = {
     "type": "AbgeordnetenCollection",
@@ -27,7 +28,7 @@ ABGEORDNETER = {
 
 def writeToJSON(data):
 	# save geoJSON to file
-	with open("data.json", "w") as file:
+	with codecs.open("data.json", "w", "latin_1") as file:
 		file.write(json.dumps(data, indent=4))
 	file.close()
 
@@ -48,7 +49,7 @@ for node in tree.xpath('//div[@class="linkIntern"]//a')[:10]:
 	abgeordneter['name_last'] = name[1]
 	url = 'http://bundestag.de/bundestag/abgeordnete18' + link.replace('..','')
 	abgeordneter['url'] = url
-	print "Reading 'Abgeordneter' from: ", abgeordneter['url']
+	# print "Reading 'Abgeordneter' from: ", abgeordneter['url']
 	page2 = requests.get(url)
 	tree2 = html.fromstring(page2.text)
 	abgeordneter['party'] = tree2.xpath('//div[@class="inhalt"]//h1/text()')[0].split(', ')[1]
@@ -61,9 +62,12 @@ for node in tree.xpath('//div[@class="linkIntern"]//a')[:10]:
 		abgeordneter['born_place'] = "Not defined"
 	try:
 		abgeordneter['voted_by'] = tree2.xpath('//div[@id="context"]//div[@class="contextBox"]//h2/text()')[4]
+		# abgeordneter['voted_by'] = abgeordneter['voted_by'].encode('latin_1').decode('utf-8')
 		abgeordneter['ward'] = tree2.xpath('//div[@id="context"]//div[@class="contextBox"]//div[@class="standardBox"]//strong/text()')[1]
 	except:
 		print abgeordneter['name'], "makes problems!"
+	# print abgeordneter['voted_by'].encode('latin_1')
+	print abgeordneter['voted_by']
 	collection["abgeordnete"].append(abgeordneter)
 
 writeToJSON(collection)
