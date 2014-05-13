74110b74cde83e106d5e759ff80564d4fb7e4bba
milkbread <milkbread@freenet.de>
2014-05-13 11:16:30 +0200

    Read basic info of Abgeordnete

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 
 
+[Basics](http://docs.python-guide.org/en/latest/scenarios/scrape/)
 [get HREF attribute](http://stackoverflow.com/questions/14119590/scraperwiki-lxml-how-to-get-the-href-attribute-of-a-child-of-an-element-with)
--- scrap.py
+++ scrap.py
@@ -1,5 +1,27 @@
 from lxml import html
 import requests
+import json
+import copy
+
+COLLECTION = {
+    "type": "AbgeordnetenCollection",
+    "abgeordnete": [],
+    "properties": {}
+}
+
+ABGEORDNETER = {
+	"name": "",
+	"party": -1,
+	"born_date": "",
+	"born_place": "",
+	"profession": "",
+	"url": "",
+	"voted_by": "",
+	"ward": "",
+	"type": "Abgeordneter"
+}
+
+partys = []
 
 page = requests.get('http://bundestag.de/bundestag/abgeordnete18/alphabet/index.html')
 tree = html.fromstring(page.text)
@@ -11,6 +33,19 @@ tree = html.fromstring(page.text)
 
 
 
-link = tree.xpath('//div[@class="linkIntern"]//a//@href')[0]
+link = tree.xpath('//div[@class="linkIntern"]//a//@href')[1]
 url = 'http://bundestag.de/bundestag/abgeordnete18' + link.replace('..','')
-print url
+abgeordneter = copy.deepcopy(ABGEORDNETER)
+abgeordneter['url'] = url
+page2 = requests.get(url)
+tree2 = html.fromstring(page2.text)
+heading = tree2.xpath('//div[@class="inhalt"]//h1/text()')[0].split(', ')
+abgeordneter['name'] = heading[0]
+abgeordneter['party'] = heading[1]
+abgeordneter['profession'] = tree2.xpath('//div[@class="inhalt"]//p//strong/text()')[0].split(', ')
+born = tree2.xpath('//div[@class="inhalt"]//p/text()')[1].replace('Geboren am ', '').split(' in ')
+abgeordneter['born_date'] = born[0]
+abgeordneter['born_place'] = born[1].split(';')[0]
+abgeordneter['voted_by'] = tree2.xpath('//div[@id="context"]//div[@class="contextBox"]//h2/text()')[4]
+abgeordneter['ward'] = tree2.xpath('//div[@id="context"]//div[@class="contextBox"]//div[@class="standardBox"]//strong/text()')[1]
+print abgeordneter
